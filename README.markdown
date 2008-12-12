8d97bdba5451a834f890095360897fbfd2954471
John Mair <jrmair@gmail.com>
2008-12-13 08:22:07 +1300

    gitted

---
+++ README.markdown
@@ -0,0 +1,6 @@
+Window-rotate
+=============
+
+* rotate windows according to cyclic ordering of windows.
+* i.e buffer of last window becomes buffer of first window, and so on.
+* active by: M-x rotate-windows, or set a key binding for ease of use.
---
+++ window-rotate.el
@@ -0,0 +1,7 @@
+(defun rotate-windows-helper(x d)
+  (if (equal (cdr x) nil) (set-window-buffer (car x) d)
+    (set-window-buffer (car x) (window-buffer (cadr x))) (rotate-windows-helper (cdr x) d)))
+ 
+(defun rotate-windows ()
+  (interactive)
+  (rotate-windows-helper (window-list) (window-buffer (car (window-list)))))
