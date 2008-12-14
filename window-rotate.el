2c11d3d62917b19f8e4299dd4847e71402930664
John Mair <jrmair@gmail.com>
2008-12-14 20:56:10 +1300

    select-window now follows the buffer

--- window-rotate.el
+++ window-rotate.el
@@ -4,4 +4,8 @@
  
 (defun rotate-windows ()
   (interactive)
-  (rotate-windows-helper (window-list) (window-buffer (car (window-list)))))
+  (rotate-windows-helper (window-list) (window-buffer (car (window-list))))
+  (select-window (car (last (window-list)))))
+
+; bind to the key super-r 
+(global-set-key (kbd "s-r") 'rotate-windows)
